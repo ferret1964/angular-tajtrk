79791159c663a7028d64c030b0e585a15f6dee03
ferret1964 <ferret@stormwoods.com>
2020-03-26 14:17:02 -0700

    3 files modified, 1 added

--- src/app/product-alerts/product-alerts.component.ts
+++ src/app/product-alerts/product-alerts.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Input } from '@angular/core';
 
 @Component({
   selector: 'app-product-alerts',
@@ -8,6 +9,8 @@ import { Component, OnInit } from '@angular/core';
 export class ProductAlertsComponent implements OnInit {
 
   constructor() { }
+
+  @Input() product;
 
   ngOnInit() {
   }
