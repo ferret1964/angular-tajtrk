7a09f6fb3c7f8b49d1c2cc935f57528de9988468
ferret1964 <ferret@stormwoods.com>
2020-03-26 15:28:58 -0700

    2 files modified

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -14,13 +14,17 @@ export class ProductDetailsComponent implements OnInit {
 
   product;
 
-  constructor( private route: ActivatedRoute,) {  
+  constructor( private route: ActivatedRoute, private cartService: CartService) {  
   }
 
   ngOnInit() {
     this.route.paramMap.subscribe(params => {
     this.product = products[+params.get('productId')];
   });
+  }
+    addToCart(product) {
+    this.cartService.addToCart(product);
+    window.alert('Your product has been added to the cart!');
   }
 
 }
